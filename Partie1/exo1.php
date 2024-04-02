776cfa6016bb762ada81dac05ecc66840b101c3a
Ayaddouz Hajar <hajarayaddouz29@gmail.com>
2024-04-02 15:38:41 +0200

    exo 2 php

--- Partie1/exo1.php
+++ Partie1/exo1.php
@@ -1,13 +1,13 @@
 <h1> Exercice 1 </h1>
 
 <p>Soit la phrase "Notre formation DL commence aujourd'hui." <br>
-Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase</p>
+Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase (espaces inclus). </p>
 
 <h2>Résultat</h2>
 
 <?php
 
-$phrase = "Notre Dl commence aujourd'hui";
+$phrase = "Notre formation Dl commence aujourd'hui";
 $nbCaracteres = strlen($phrase);
 echo "La phrase contient $nbCaracteres caractères";
 
