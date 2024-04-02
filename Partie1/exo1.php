74748bcf16557885f0d6ee36662b4e08dc1d4604
Ayaddouz Hajar <hajarayaddouz29@gmail.com>
2024-04-02 14:25:44 +0200

    premier exercice php

---
+++ Partie1/exo1.php
@@ -0,0 +1,13 @@
+<h1> Exercice 1 </h1>
+
+<p>Soit la phrase "Notre DL commence aujourd'hui." <br>
+Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$phrase = "Notre Dl commence aujourd'hui";
+$nbCaracteres = strlen($phrase);
+echo "La phrase contient $nbCaracteres caractères";
+
