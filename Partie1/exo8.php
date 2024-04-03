d0a354270088d8eb95d21ce5f91aafd80c8d33e1
Ayaddouz Hajar <hajarayaddouz29@gmail.com>
2024-04-03 14:37:33 +0200

    exo 8

---
+++ Partie1/exo8.php
@@ -0,0 +1,28 @@
+<h1> Exercice 8</h1>
+
+<p>Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé en paramètre sous la
+forme : <br> Table de 8 :
+1 x 8 = 8 <br>
+2 x 8 = 16 <br>
+3 x 8 = 24 … <br>
+</p>
+<h2>Remarque : proposer 2 solutions avec 2 types de boucles.</h2>
+
+
+ <?php
+
+$nbr=8;
+
+for($i = 1; $i <= 10; $i++) {
+    $resultat=$nbr*$i;
+    echo " $i x $nbr = $resultat <br>";
+}
+
+$j = 1;
+$nbr=6;
+while($j <= 10) {
+    $resultat= $j*$nbr;
+    echo "<br> $j x $nbr = $resultat <br>";
+    $j++;
+}
+
