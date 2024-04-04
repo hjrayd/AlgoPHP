115130d8eb0a35ad3fbf612132760009d0deee2c
Ayaddouz Hajar <hajarayaddouz29@gmail.com>
2024-04-04 15:24:27 +0200

    exo11

---
+++ Partie1/exo11.php
@@ -0,0 +1,19 @@
+<h1>Exercice 11</h1>
+
+<p>Initialiser un tableau de x marques de voitures. Ecrire un algorithme permettant de parcourir ce
+tableau et d’en afficher le contenu (une marque de voiture par ligne). Il est également demandé
+d’afficher le nombre de marques de voitures présentes dans le tableau.</p>
+
+<h2>Afficher le résultat</h2>
+
+<?php
+
+$marques = ["Peugeot", "Renault", "BMW", "Mercedes"];
+$nbmarques = count($marques);
+
+echo "Il y a $nbmarques marques de voitures dans la liste : <br>";
+
+for ($i = 0 ; $i < count($marques); $i++ ) 
+   { echo $marques[$i]. "<br>"; 
+}
+
