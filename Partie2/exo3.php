17b4b1d1b7d05bd9aeabdce25dc78a88d827448c
Ayaddouz Hajar <hajarayaddouz29@gmail.com>
2024-04-25 11:07:43 +0200

    exo 3 corrected (conflicts problems)

---
+++ Partie2/exo3.php
@@ -0,0 +1,17 @@
+<h1>Exercice 3</h1>
+
+
+<p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir 
+dans un nouvel onglet (_blank). </p>
+
+<h2> Afficher le résultat </h2>
+
+<?php
+
+$url= "https://elan-formation.fr/accueil";
+
+echo "<a href=$url target='_blank'> Elan formation </a>";
+
+?>
+
+
