d7daf7556d086820d2879cb06fd9ab1be8070bf5
Ayaddouz Hajar <hajarayaddouz29@gmail.com>
2024-04-25 09:17:34 +0200

    exo 3

---
+++ Partie2/exo3.php
@@ -0,0 +1,16 @@
+<h1>Exercice 3</h1>
+
+<p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir 
+dans un nouvel onglet (_blank)</p>
+
+<h2>Résultat</h2>
+
+<a href="https://elan-formation.fr/" target="_blank">Elan Formation</a>
+
+<?php
+
+//ou bien
+
+$url = "https://elan-formation.fr/";
+
+echo "<a href='$url' target='_blank'>Elan Formation</a>";
